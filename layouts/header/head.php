a4b87bc8520c2f9392c784e4e968f4db0f0ffae6
Hankin-han <hanhanjun888@163.com>
2019-09-27 09:30:32 +0800

    优化模板格式

--- layouts/header/head.php
+++ layouts/header/head.php
@@ -15,21 +15,18 @@ Blog：https://www.hankin.cn -->
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <title><?php show_wp_title() ?></title>
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="">
-    <?php wp_head(); ?>
-    <link rel="icon" href="<?php echo home_url(); ?>/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css?version=<?= time()?>">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css?version=<?= time()?>">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/monokai_sublime.min.css?version=<?= time()?>">
-    <link rel="stylesheet" class="layout-css" id="layout-css" href="">
-    <link rel="stylesheet" class="rtl-css" id="rtl-css" href="">
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<?php wp_head(); ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css?version=<?= time()?>">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css?version=<?= time()?>">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/monokai_sublime.min.css?version=<?= time()?>">
+<link rel="icon" href="<?php echo home_url(); ?>/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" class="layout-css" id="layout-css" href="">
+<link rel="stylesheet" class="rtl-css" id="rtl-css" href="">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="" />
+<meta name="keywords" content="">
 </head>
 <body class="container box-layout background-img-1">
 <!-- [ Pre-loader ] start -->
