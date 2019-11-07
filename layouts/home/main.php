2d937cd13979720406c50753a64735f688d247e7
Hankin-han <hanhanjun888@163.com>
2019-11-07 10:14:06 +0800

    处理公告新标签页打开BUG

--- layouts/home/main.php
+++ layouts/home/main.php
@@ -10,7 +10,7 @@ $i_notice = cs_get_option('i_notice'); //自定义友情链接
                 <div class="notice breadcrumb" id="notice">
                     <ul class="notice-list" style="margin-top: 0px;">
                             <?php foreach($i_notice as $notice):?>
-                                <li><a href="<?= $notice['i_notice_link']?>" target="_blank" title="<?= $notice['i_notice_title']?>"><i class="feather icon-volume-2"></i><?= $notice['i_notice_title']?></a></li>
+                                <li><a href="<?= $notice['i_notice_link']?>" <?php if($notice['i_notice_newtab']):?>target="_blank"<?php endif;?> title="<?= $notice['i_notice_title']?>"><i class="feather icon-volume-2"></i><?= $notice['i_notice_title']?></a></li>
                             <?php endforeach;?>     
                     </ul>
                 </div>
