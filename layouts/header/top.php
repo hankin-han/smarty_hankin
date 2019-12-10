421a765ac3212c740fcaecd76c143500877b6e96
Hankin-han <hanhanjun888@163.com>
2019-12-10 17:35:14 +0800

    处理移动端 顶部悬浮

--- layouts/header/top.php
+++ layouts/header/top.php
@@ -2,7 +2,7 @@
 $i_avatar_logo = cs_get_option('i_avatar_logo'); //自定义友情链接
 ?>
 <!-- [ Header ] start -->
-<header class="navbar pcoded-header navbar-expand-lg navbar-light header-blue">
+<header class="navbar pcoded-header navbar-expand-lg navbar-light header-blue <?php if(wp_is_mobile()):?> headerpos-fixed <?php endif;?>">
     <div class="m-header">
         <a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
         <a href="<?= home_url()?>" class="b-brand">
